0380210e212e2b7d2a29191f4b6cf69010b6cb30
GreciaNav <grecianav39@gmail.com>
2020-10-26 16:38:45 -0600

    Added bonusSM to git

--- test/bonusSM/button_demo/led.c
+++ test/bonusSM/button_demo/led.c
@@ -10,15 +10,25 @@ void led_init()
 }
 
 void led_update(){
-  if (switch_state_changed) {
-    char ledFlags = 0; /* by default, no LEDs on */
+  static int state = 0;
+  char ledFlags = 0; /* by default, no LEDs on */
+  if (switch_state_changed && switch_state_down) {
 
+    if (state){
+      ledFlags |= LED_RED;
+      state = 0;
+    }
+    else {
+      ledFlags |= LED_GREEN;
+      state = 1;
+    }
+    /*
     ledFlags |= switch_state_down ? LED_GREEN : 0;
     ledFlags |= switch_state_down ? 0 : LED_RED;
-
+    */
     P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
     P1OUT |= ledFlags;         // set bits for on leds
+    switch_state_changed = 0;
   }
-  switch_state_changed = 0;
 }
 
