0c85ea3487d6f292360ed66e2703045524a1b1f9
GreciaNav <grecianav39@gmail.com>
2020-10-26 16:02:03 -0600

    Finished buzz_song_advance() in stateMachines.c

---
+++ project/src/notes.h
@@ -0,0 +1,11 @@
+#ifndef notes_included
+#define notes_included
+
+#define G 2551
+#define E 3033
+#define F 2863
+#define Cn 3822
+#define ASH 4290
+#define D 3405
+
+#endif
---
+++ project/src/stateMachines.h
@@ -0,0 +1,7 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void buzzer_advance();
+void main_state_advance();
+void buzz_song_advance();
+#endif // included
